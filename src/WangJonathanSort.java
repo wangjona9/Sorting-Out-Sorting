01c3a5ba6f6927864a8085ed639ce67d774d25c4
wangjona9 <118570694+wangjona9@users.noreply.github.com>
2023-11-02 20:35:18 -0500

    Update WangJonathanSort.java

--- src/WangJonathanSort.java
+++ src/WangJonathanSort.java
@@ -13,66 +13,54 @@ import java.util.List;
 
 public class WangJonathanSort implements Sorter {
 
-    // +--------+------------------------------------------------------
-    // | Fields |
-    // +--------+
+  /**
+   * The one sorter you can access.
+   */
+  public static Sorter SORTER = new WangJonathanSort();
 
-    /**
-     * The one sorter you can access.
-     */
-    public static Sorter SORTER = new WangJonathanSort();
+  /**
+   * Create a sorter.
+   *
+   * @param <T>
+   */
+  public static <T> void wangJonathanSort(T[] vals, Comparator<? super T> comparator) {
+    int n = vals.length;
 
-    // +--------------+------------------------------------------------
-    // | Constructors |
-    // +--------------+
+    // Create empty buckets
+    List<List<T>> buckets = new ArrayList<>(n);
 
-    /**
-     * Create a sorter.
-     *
-     * @param <T>
-     */
-    public static <T> void wangJonathanSort(T[] vals, Comparator<? super T> comparator) {
-        int n = vals.length;
-
-        // Create empty buckets
-        List<List<T>> buckets = new ArrayList<>(n);
-
-        for (int i = 0; i < n; i++) {
-            buckets.add(new ArrayList<>());
-        }
-
-        // Find the minimum and maximum values in vals
-        T minVal = Arrays.stream(vals).min(comparator).orElse(null);
-        T maxVal = Arrays.stream(vals).max(comparator).orElse(null);
-
-        if (minVal == null || maxVal == null) {
-            return; // Return early if either minVal or maxVal is null
-        }
-
-        // Place elements into buckets
-        for (T val : vals) {
-            int index = (int) ((n - 1) * ((double) (comparator.compare(val, minVal) - comparator.compare(val, maxVal))
-                    / (comparator.compare(maxVal, minVal))));
-            index = Math.max(0, Math.min(index, n - 1)); // Ensure index is within valid range
-            buckets.get(index).add(val);
-        }
-
-        // Sort each bucket and reconstruct the sorted array
-        int index = 0;
-        for (List<T> bucket : buckets) {
-            Collections.sort(bucket, comparator);
-            for (T val : bucket) {
-                vals[index++] = val;
-            }
-        }
+    for (int i = 0; i < n; i++) {
+      buckets.add(new ArrayList<>());
     }
 
-    // +---------+-----------------------------------------------------
-    // | Methods |
-    // +---------+
+    // Find the minimum and maximum values in vals
+    T minVal = Arrays.stream(vals).min(comparator).orElse(null);
+    T maxVal = Arrays.stream(vals).max(comparator).orElse(null);
 
-    @Override
-    public <T> void sort(T[] values, Comparator<? super T> order) {
-        wangJonathanSort(values, order);
+    if (minVal == null || maxVal == null) {
+      return; // Return early if either minVal or maxVal is null
     }
+
+    // Place elements into buckets
+    for (T val : vals) {
+      int index = (int) ((n - 1) * ((double) (comparator.compare(val, minVal) - comparator.compare(val, maxVal))
+              / (comparator.compare(maxVal, minVal))));
+      index = Math.max(0, Math.min(index, n - 1)); // Ensure index is within valid range
+      buckets.get(index).add(val);
+    }
+
+    // Sort each bucket and reconstruct the sorted array
+    int index = 0;
+    for (List<T> bucket : buckets) {
+      Collections.sort(bucket, comparator);
+      for (T val : bucket) {
+        vals[index++] = val;
+      }
+    }
+  }
+
+  @Override
+  public <T> void sort(T[] values, Comparator<? super T> order) {
+    wangJonathanSort(values, order);
+  }
 }
